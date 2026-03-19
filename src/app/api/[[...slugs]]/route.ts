6c61ba629ebab366015ed5fe24dc503e2561e81a
Delon Pratama <delonpratama1009@gmail.com>
2026-03-19 17:34:45 +0700

    first commit

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,8 @@
+import { Elysia, t } from "elysia";
+
+export const app = new Elysia({ prefix: "/api" }).get("/user", {
+  user: { name: "John" },
+});
+
+export const GET = app.fetch;
+export const POST = app.fetch;
